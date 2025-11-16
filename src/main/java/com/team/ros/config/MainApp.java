fa6124b8db7c55167c7d3512a87efd993e33867a
KasperSaint07 <123306320+KasperSaint07@users.noreply.github.com>
2025-11-16 18:41:50 +0500

    fix

--- src/main/java/com/team/ros/config/MainApp.java
+++ src/main/java/com/team/ros/config/MainApp.java
@@ -2,10 +2,16 @@ package com.team.ros.config;
 import com.team.ros.cli.UserUI;
 import com.team.ros.cli.CashierUI;
 import com.team.ros.notify.NotifierSetup;
+
 public class MainApp {
     public static void main(String[] args) {
-        NotifierSetup.initDefault();
-        new CashierUI().start();
-        new UserUI().start();
+        System.out.println("Start as:");
+        System.out.println("1) User UI");
+        System.out.println("2) Cashier UI");
+        System.out.print("Choose: ");
+        java.util.Scanner in = new java.util.Scanner(System.in);
+        String s = in.nextLine().trim();
+        if ("1".equals(s)) new com.team.ros.cli.UserUI().start();
+        else new com.team.ros.cli.CashierUI().start();
     }
 }
